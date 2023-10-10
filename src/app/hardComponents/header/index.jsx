014153a6cb2fea5a63a5a3c21e5187d10f4753de
ngoc99hp <ngoc99hp@gmail.com>
2023-10-10 09:31:14 +0700

    header

--- src/app/hardComponents/header/index.jsx
+++ src/app/hardComponents/header/index.jsx
@@ -1,8 +1,18 @@
+// 'use client'
+
+import Image from 'next/image'
+import { FcMenu } from 'react-icons/fc';
 export default async function Index() {
     return(
-        <div>
-            <img src="/logo.png" alt="" className="w-[100px] md:w-[150px] h-[100px] object-scale-down cursor-pointer"/>
-            Header
+        <div className="flex items-center border-b-2 h-[80px] w-[100%]">
+            <FcMenu/>
+            <div className="flex">
+                <Image src="/logo2.png" alt="" width="70" height="70" className="w-[70px] h-[70px] object-scale-down cursor-pointer"/>
+                <p className="flex flex-col items-center justify-center">
+                    <span>HUU NGHI</span>
+                    <span>SCHOOL</span>
+                </p>
+            </div>
         </div>
     );
 }
